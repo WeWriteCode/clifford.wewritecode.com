eda0d232574a8dba4fc4769a34a71b6339f0b615
Tyler Riker <tyler@wewritecode.com>
2025-07-10 17:05:19 -0500

    Assure running with SUDO; switch to ec2-user for UV install

--- run.sh
+++ run.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
+# Ensure the script is run as root
+if [ "$(id -u)" -ne 0 ]; then
+  echo "This script must be run with sudo"
+  exit 1
+fi
+
 echo "Update system"
 dnf update
 dnf upgrade --releasever=2023.7.20250331 -y
 dnf groupinstall "Development Tools" -y
 #dnf install rust cargo -y
 
+# Switch to ec2-user and perform installation of UV
+sudo -u ec2-user -i <<EOF
 echo "Install UV"
 curl -LsSf https://astral.sh/uv/install.sh | sh
+EOF
 
 echo "Install open-webui"
 ./install-open-webui.sh
